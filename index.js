d5a555d5e9fcea27eca56d1f27ded5499814b0b3
unknown <dasha.nebogina@mail.ru>
2025-03-03 17:00:13 +0300

    feat: implement changing active class button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let content = document.getElementsByClassName("text__content");
+let button = document.getElementsByClassName("btn__tab");
 let pics = document.getElementsByClassName("picture__wrapper")[0];
 
 content[1].style.display = "none";
@@ -7,32 +8,25 @@ content[2].style.display = "none";
 pics.children[1].style.display = "none";
 pics.children[2].style.display = "none";
 
+function show(num) {
+    content[num].style.display = "flex";
+    pics.children[num].style.display = "flex";
+    button[num].classList.add("active");
+}
+
+function hide(num) {
+    for (let i = 0; i < 3; i += 1) {
+        if (num !== i) {
+            content[i].style.display = "none";
+
+            pics.children[i].style.display = "none";
+
+            button[i].classList.remove("active");
+        }
+    }
+}
+
 function openTab(num) {
-    if (num === 1) {
-        content[0].style.display = "flex";
-        content[1].style.display = "none";
-        content[2].style.display = "none";
-
-        pics.children[0].style.display = "flex";
-        pics.children[1].style.display = "none";
-        pics.children[2].style.display = "none";
-    }
-    else if (num === 2) {
-        content[1].style.display = "flex";
-        content[0].style.display = "none";
-        content[2].style.display = "none";
-
-        pics.children[1].style.display = "flex";
-        pics.children[0].style.display = "none";
-        pics.children[2].style.display = "none";
-    }
-    else if (num === 3) {
-        content[2].style.display = "flex";
-        content[0].style.display = "none";
-        content[1].style.display = "none";
-
-        pics.children[2].style.display = "flex";
-        pics.children[0].style.display = "none";
-        pics.children[1].style.display = "none";
-    }
+    show(num);
+    hide(num);
 }
